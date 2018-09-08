66a12b368c1adadfb3df714c21a01266cbd3b4ae
TOSUKUi <am.businessmail.exp@gmail.com>
2018-09-08 20:07:57 +0900

    bench.shにおいてベンチ対象のホストを引数で指定できるようにした

--- isucon4-qualifier/bench.sh
+++ isucon4-qualifier/bench.sh
@@ -1,1 +1,7 @@
-docker-compose --file "docker-compose-${1}.yml" up bench
+#!/bin/bash
+if [ $# -ne 2 ]; then
+    echo "Usage: ./bench <target language> <target hostname or ip>"
+    exit 1
+fi
+docker-compose --file "docker-compose-${1}.yml" build bench; 
+docker-compose --file "docker-compose-${1}.yml" run --entrypoint "sudo -u isucon -i ./benchmarker bench --host ${2}" bench;
