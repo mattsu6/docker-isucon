53b6de16acb5ba8abd4f9caf73cc49507d5f68f5
TOSUKUi <am.businessmail.exp@gmail.com>
2018-09-14 18:35:01 +0900

    add branch targetting

--- isucon4-qualifier/bench.sh
+++ isucon4-qualifier/bench.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [ $# -ne 2 ]; then
-    echo "Usage: ./bench <target language> <target hostname or ip>"
+if [ $# -ne 3 ]; then
+    echo "Usage: ./bench <target language> <target hostname or ip> <target_branch>"
     exit 1
 fi
-docker-compose --file "docker-compose-${1}.yml" build bench; 
+docker-compose --file "docker-compose-${1}.yml" build bench --build_args target_branch=${3}; 
 docker-compose --file "docker-compose-${1}.yml" run --entrypoint "sudo -u isucon -i ./benchmarker bench --host ${2}" bench;
