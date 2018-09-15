97ceedff41fe2d4c3b534ab7d6620bf871ecd4ba
TOSUKUi <am.businessmail.exp@gmail.com>
2018-09-15 17:42:21 +0900

    modified build args

--- isucon4-qualifier/bench.sh
+++ isucon4-qualifier/bench.sh
@@ -3,5 +3,5 @@ if [ $# -ne 3 ]; then
     echo "Usage: ./bench <target language> <target hostname or ip> <target_branch>"
     exit 1
 fi
-docker-compose --file "docker-compose-${1}.yml" build bench --build_args target_branch=${3}; 
+docker-compose --file "docker-compose-${1}.yml" build --build-arg target_branch=${3} bench;
 docker-compose --file "docker-compose-${1}.yml" run --entrypoint "sudo -u isucon -i ./benchmarker bench --host ${2}" bench;
